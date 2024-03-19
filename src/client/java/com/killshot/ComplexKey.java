c5ef0f276190926207eac445a9f8ea94df9fd91f
Micah Pierson <micah.pierson@outlook.com>
2024-03-18 20:04:16 -0600

    Separation of classes and organization

---
+++ src/client/java/com/killshot/ComplexKey.java
@@ -0,0 +1,111 @@
+package com.killshot;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
+
+public class ComplexKey {
+    private List<KeyBinding> complexBinding;
+    private KeyBinding simpleBinding;
+    private boolean isComplex;
+
+    ComplexKey(List<KeyBinding> complexBinding) throws KillshotException {
+        if (complexBinding.isEmpty()) {
+            throw new KillshotException("While registering keybinding: ", "binding was empty");
+        }
+
+        this.complexBinding = complexBinding;
+        this.isComplex = complexBinding.size() > 1;
+
+        if (!isComplex) {
+            this.simpleBinding = complexBinding.get(0);
+        }
+    }
+
+    ComplexKey(KeyBinding simpleBinding) {
+        this.complexBinding = null;
+        this.simpleBinding = simpleBinding;
+        this.isComplex = false;
+    }
+
+    static String getDefaultTranslationName() {
+        return "key.killshot.kill";
+    }
+
+    static InputUtil.Type getDefaultType() {
+        return InputUtil.Type.KEYSYM;
+    }
+
+    static int getDefaultKeycode() {
+        return GLFW.GLFW_KEY_COMMA;
+    }
+
+    private static KeyBinding getDefaultBaseBinding() {
+        return new KeyBinding(
+                getDefaultTranslationName(),
+                getDefaultType(),
+                getDefaultKeycode(),
+                "category.example.test"
+        );
+    }
+
+    static ComplexKey getDefaultBinding() {
+        KeyBinding defaultBase = getDefaultBaseBinding();
+        return new ComplexKey(defaultBase);
+    }
+
+    ComplexKey register() {
+        if (isComplex()) {
+            for (KeyBinding binding : complexBinding) {
+                KeyBindingHelper.registerKeyBinding(binding);
+            }
+        } else {
+            KeyBindingHelper.registerKeyBinding(simpleBinding);
+        }
+
+        return this;
+    }
+
+    ComplexKey deregister() throws KillshotException {
+        throw new KillshotException("com.killshot.ComplexKey::deregister not implemented yet!", "");
+    }
+
+    private boolean complexIsPressed() {
+        return complexBinding.stream().allMatch(KeyBinding::isPressed);
+    }
+
+    private boolean complexWasPressed() {
+        return complexBinding.stream().allMatch(KeyBinding::wasPressed);
+    }
+
+    private boolean simpleIsPressed() {
+        return simpleBinding.isPressed();
+    }
+
+    private boolean simpleWasPressed() {
+        return simpleBinding.wasPressed();
+    }
+
+    boolean isComplex() {
+        return isComplex;
+    }
+
+    boolean isPressed() {
+        if (isComplex()) {
+            return complexIsPressed();
+        }
+
+        return simpleIsPressed();
+    }
+
+    boolean wasPressed() {
+        if (isComplex()) {
+            return complexWasPressed();
+        }
+
+        return simpleWasPressed();
+    }
+}
