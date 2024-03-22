71c066f4ab34a66c810321b66c446ff3dfcb3066
Micah Pierson <micah.pierson@outlook.com>
2024-03-21 18:37:00 -0600

    Minecraft does not natively support keybindings with modifiers, so I removed some things in ComplexKey.java.

--- src/client/java/com/killshot/ComplexKey.java
+++ src/client/java/com/killshot/ComplexKey.java
@@ -5,33 +5,11 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-import java.util.List;
-
 public class ComplexKey {
-    private List<KeyBinding> complexBinding;
     private final KeyBinding simpleBinding;
-    private final boolean isComplex;
-
-    ComplexKey(List<KeyBinding> complexBinding) throws KillshotException {
-        if (complexBinding.isEmpty()) {
-            throw new KillshotException("While registering keybinding: ", "binding was empty");
-        }
-
-        this.complexBinding = complexBinding;
-        this.isComplex = complexBinding.size() > 1;
-
-        if (!isComplex) {
-            this.simpleBinding = complexBinding.get(0);
-            this.complexBinding = null;
-        } else {
-            this.simpleBinding = null;
-        }
-    }
 
     ComplexKey(KeyBinding simpleBinding) {
-        this.complexBinding = null;
         this.simpleBinding = simpleBinding;
-        this.isComplex = false;
     }
 
     static String getDefaultBindingName() {
@@ -61,27 +39,12 @@ public class ComplexKey {
     }
 
     ComplexKey register() {
-        if (isComplex()) {
-            for (KeyBinding binding : complexBinding) {
-                KeyBindingHelper.registerKeyBinding(binding);
-            }
-        } else {
-            KeyBindingHelper.registerKeyBinding(simpleBinding);
-        }
-
+        KeyBindingHelper.registerKeyBinding(simpleBinding);
         return this;
     }
 
     ComplexKey deregister() throws KillshotException {
         throw new KillshotException("com.killshot.ComplexKey::deregister not implemented yet!", "");
-    }
-
-    private boolean complexIsPressed() {
-        return complexBinding.stream().allMatch(KeyBinding::isPressed);
-    }
-
-    private boolean complexWasPressed() {
-        return complexBinding.stream().allMatch(KeyBinding::wasPressed);
     }
 
     private boolean simpleIsPressed() {
@@ -92,23 +55,11 @@ public class ComplexKey {
         return simpleBinding.wasPressed();
     }
 
-    boolean isComplex() {
-        return isComplex;
-    }
-
     boolean isPressed() {
-        if (isComplex()) {
-            return complexIsPressed();
-        }
-
         return simpleIsPressed();
     }
 
     boolean wasPressed() {
-        if (isComplex()) {
-            return complexWasPressed();
-        }
-
         return simpleWasPressed();
     }
 }
