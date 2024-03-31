c85b1299f2ffd0736d50db76130c17fe81a77005
Micah Pierson <micah.pierson@outlook.com>
2024-03-30 21:26:31 -0600

    Added some things to KillshotException

--- src/client/java/com/killshot/KillshotException.java
+++ src/client/java/com/killshot/KillshotException.java
@@ -1,13 +1,24 @@
 package com.killshot;
 
 public class KillshotException extends Throwable {
-    String prologue = "";
-    String message = "";
-    String finalMessage = "";
+    public final String prologue;
+    public final String message;
+    public final String finalMessage;
 
-    KillshotException(final String prologue, final String message) {
+    public KillshotException(final String finalMessage) {
+        this.finalMessage = finalMessage;
+        this.prologue = "";
+        this.message = "";
+    }
+
+    public KillshotException(final String prologue, final String message) {
         this.prologue = prologue;
         this.message = message;
         this.finalMessage = prologue + message;
+    }
+
+    @Override
+    public String getMessage() {
+        return finalMessage;
     }
 }
